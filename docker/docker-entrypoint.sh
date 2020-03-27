2077462141f67c8d31b1d88b36a4496c3b9b7710
Andrey Chalov <andrchalov@gmail.com>
2020-03-27 09:46:40 +0300

    version 1.0.8

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,12 +4,11 @@ USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-9001}
 
 if [ ! -d "/home/user" ]; then
-  addgroup --gid $GROUP_ID usergrp
-  adduser --gecos "" --shell /bin/sh --home /home/user --uid $USER_ID --disabled-password --ingroup usergrp user
+  addgroup -g $GROUP_ID usergrp
+  adduser -s /bin/sh -h /home/user -u $USER_ID -D -G usergrp user
 fi
 
 export HOME=/home/user
 
 cd /opt/app
-
-exec gosu user "$@"
+exec su-exec user "$@"
